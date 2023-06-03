d2328a33294888baa5badf8166f455cc814d9e6a
mintol <juna216@naver.com>
2023-06-03 16:10:44 +0900

    Adress 추가 설정

--- src/main/java/jpabook/jpashop/domain/Adress.java
+++ src/main/java/jpabook/jpashop/domain/Adress.java
@@ -6,9 +6,17 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-public class Adress {
+public class Adress { //data 타입은 값이 변경되면 안되기 때문에 setter 설정 X
     private String city;
     private String street;
     private String zipcode;
 
+    protected Adress() {
+    }
+
+    public Adress(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
